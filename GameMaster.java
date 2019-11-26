51d906da5170a3f2ce075d1b12422dddfb986a30
Stefan Ganze <stefan.ganze@googlemail.com>
2019-11-26 16:45:45 +0100

    erst Abhandlung

---
+++ GameMaster.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+public class GameMaster{
+
+  //Dealer dealer;
+  //Player player;
+
+  /*public GameMaster(Dealer d, Player p){
+    this.dealer = d;
+    this.player = p;
+  }*/
+
+  public int start(){
+
+      Scanner scanner = new Scanner(System.in);
+      int auswahl;
+      System.out.println("*****************************************************\nHerzlich Willkommen zu einer Runde Blackjack!!!!\n*****************************************************");
+      System.out.println("Möchten Sie ein neues Spiel starten dann drücken sie die 1!");
+      System.out.println("zum Beenden drücken Sie bitte die 2.");
+
+      auswahl = scanner.nextInt();
+      return auswahl;
+  }
+
+  public boolean playAGame(){
+
+    int dealer=0;
+    int player=0;
+
+    //Runde 1
+    dealer += cd.getCard();
+    player += cd.getCard();
+    System.out.println("Runde 1!");
+    System.out.println("Dealer: "+dealer);
+    System.out.println("Player: "+player);
+
+    //Runde 2
+    dealer += cd.getHiddenCard();
+    player += cd.getCard();
+    System.out.println("Runde 2!");
+    System.out.println("Dealer: "+dealer);
+    System.out.println("Player: "+player);
+
+    if(player==21){
+      System.out.println("Herzlichen Glückwunsch");
+      return true;
+    }
+
+    if(player == 22){
+      player=12;
+    }
+
+    //Ab Runde 3 spielt der Spieler
+    int auswahl = 1;
+    while(auswahl!=2){
+      System.out.println("Möchten sie weiter spien? Ihr akuteller Kartenwert beträgt: "+player);
+      System.out.println("1. Neue Karte! \n2. Keine Karte!");
+      Scanner scanner = new Scanner(System.in);
+      auswahl = scanner.nextInt();
+      if(auswahl == 1){
+        player += cd.getCard();
+      }
+      if(player == 21){
+        System.out.println("Sie haben gewonnen");
+        return true;
+      }
+      if(player > 21){
+        System.out.println("Sie haben verloren");
+        return false;
+      }
+    // Dealer spielt nachdem alle gespielt haben
+    while(dealer<17){
+      dealer += cd.getCard;
+    }
+  }
+
+
+    System.out.println("Sie haben verloren");
+  }
+}
--- Play.java
+++ Play.java
@@ -2,5 +2,14 @@ public class Play{
 
   public static void main(String[] args){
 
+    //Dealer d;
+    //Player p;
+      GameMaster gm = new GameMaster();
+
+      if(gm.start()==2){
+        System.out.println("Vielen Dank für Ihre Teilnahme!!!!");
+      }else{
+        gm.playAGame();
+      }
   }
 }
