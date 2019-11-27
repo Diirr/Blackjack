e5dd4f175e3a57fd3ddb62195b29e6b70bdb7835
Stefan Ganze <stefan.ganze@googlemail.com>
2019-11-27 14:25:25 +0100

    der letzte Stand unseres Spiels...soweit

--- GameMaster.java
+++ GameMaster.java
@@ -14,6 +14,54 @@ public class GameMaster{
       return auswahl;
   }
 
+  public String printWait(int s, int str, int x, int y) throws Exception {
+
+    String strDefault = "Fehler 404";
+    String str1 = "Der Dealer hat eine "+x+" erhalten. Der aktuelle Kartenwert des Dealers beträgt "+y+".";
+    String str2 = "Sie haben eine "+x+" erhalten. Ihr aktueller Kartenwert beträgt "+y+".\n";
+    String str3 = "Der Dealer hat eine verdeckte Karte erhalten. Der aktuelle Kartenwert des Dealers beträgt "+x+".";
+    String str4 = "Herzlichen Glückwunsch - BlackJack!!!\n";
+    String str5 = "Der Dealer verliert mit "+x+". Sie gewinnen mit "+y+" Augen.\n\n";
+    String str6 = "Der Dealer gewinnt mit "+x+". Sie verlieren mit "+y+" Augen.\n\n";
+    String str7 = "Der Dealer hat "+x+" Augen. \n";
+    String str8 = "Der Dealer zieht eine Karte";
+    String str9 = "Sie haben sich für eine "+x+" entschieden. Ihr neuer Kartenwert beträgt: "+y+"\n";
+    String str10 = "Möchten sie weiter spielen? Ihr akuteller Kartenwert beträgt: "+x+ "\n\n1. Neue Karte! \n2. Keine Karte!\n";
+    String str11 = "Sie haben ein Ass erhalten. Soll es als 1 oder 11 gewertet werden?\n\n 1. Ass als 1 werten! \n2. Ass als 11 werten!\n";
+    String str12 = "Unentschieden. Beide haben "+x+" Augen.\n\n";
+
+    Thread.sleep(s*1000);
+
+    switch(str){
+      case 1:
+          return str1;
+      case 2:
+          return str2;
+      case 3:
+          return str3;
+      case 4:
+          return str4;
+      case 5:
+          return str5;
+      case 6:
+          return str6;
+      case 7:
+          return str7;
+      case 8:
+          return str8;
+      case 9:
+          return str9;
+      case 10:
+          return str10;
+      case 11:
+          return str11;
+      case 12:
+          return str12;
+      default:
+          return strDefault;
+    }
+  }
+
   public boolean playAGame() throws Exception{
 
     CardDeck cd = new CardDeck();
@@ -31,10 +79,8 @@ public class GameMaster{
     dealerSecond = dealerFirst;
     player += tmpPlayer;
 
-    System.out.println("Der Dealer hat eine "+tmpDealer+" erhalten. Der aktuelle Kartenwert beträgt "+dealerFirst+".");
-    Thread.sleep(2000);
-    System.out.println("Sie haben eine "+tmpPlayer+" erhalten. Der aktuelle Kartenwert beträgt "+player+".\n");
-    Thread.sleep(2000);
+    System.out.println(printWait(2,1,tmpDealer, dealerFirst));
+    System.out.println(printWait(2,2, tmpPlayer, player));
 
     //Runde 2
     tmpDealer = cd.getCard();
@@ -47,13 +93,11 @@ public class GameMaster{
       player=12;
     }
 
-    System.out.println("Der Dealer hat eine verdeckte Karte erhalten. Der aktuelle Kartenwert beträgt "+dealerFirst+".");
-    Thread.sleep(2000);
-    System.out.println("Sie haben eine "+tmpPlayer+" erhalten. Der aktuelle Kartenwert beträgt "+player+".\n");
-    Thread.sleep(3000);
+    System.out.println(printWait(2,3,dealerFirst,0));
+    System.out.println(printWait(2,2,tmpPlayer, player));
 
     if(player==21){
-      System.out.println("Herzlichen Glückwunsch - BlackJack!!!\n");
+      System.out.println(printWait(2,4,0,0));
       return true;
     }
 
@@ -67,85 +111,73 @@ public class GameMaster{
     int elevenOrOne;
     int karte= 0;
 
-    while(auswahl!=2){
-      System.out.println("Möchten sie weiter spielen? Ihr akuteller Kartenwert beträgt: "+player);
-      System.out.println("\n1. Neue Karte! \n2. Keine Karte!\n");
+    // Weiterspielen und neue Karte ziehen oder seinen Spielzug beenden
+    while(auswahl!=2&&player<21){
+      System.out.println(printWait(1,10,player,0));
       Scanner scanner = new Scanner(System.in);
       auswahl = scanner.nextInt();
       karte = cd.getCard();
 
+      //Der Spieler zieht eine neue Karte und es kein Ass
       if(auswahl == 1 && karte != 11){
         player += karte;
-        Thread.sleep(1000);
-        System.out.println("Sie haben eine "+karte+" erhalten. Der aktuelle Kartenwert beträgt "+player+".");
+        System.out.println(printWait(2,2,karte,player));
         }
 
+      //Der Spieler zieht eine Karte und hat ein Ass gezogen
       if(auswahl == 1 && karte == 11){
-        Thread.sleep(1000);
-        System.out.println("Sie haben ein Ass erhalten. Soll es als 1 oder 11 gewertet werden?\n");
-        System.out.println("1. Ass als 1 werten! \n2. Ass als 11 werten!\n");
+        System.out.println(printWait(1,11,0,0));
         elevenOrOne  = scanner.nextInt();
 
         if(elevenOrOne == 1){
           player += 1;
-          Thread.sleep(1000);
-          System.out.println("Sie haben sich für eine 1 entschieden. Ihr neuer Kartenwert beträgt: "+player+"\n");
+          System.out.println(printWait(1,9,1,player));
         }
 
         if(elevenOrOne == 2){
           player += 11;
-          Thread.sleep(1000);
-          System.out.println("Sie haben sich für eine 11 entschieden. Ihr neuer Kartenwert beträgt: "+player+"\n");
+          System.out.println(printWait(1,9,11,player));
         }
       }
     }
 
       if(player == 21){
-        Thread.sleep(1000);
-        System.out.println("Sie haben gewonnen - BLACKJACK!!!!!!!\n\n");
+        System.out.println(printWait(2,4,0,0));
         return true;
       }
       if(player > 21){
-        Thread.sleep(1000);
-        System.out.println("Der Dealer gewinnt mit "+dealerSecond+". Sie verlieren mit "+player+" Augen.\n\n");
+        System.out.println(printWait(2,6,dealerSecond, player));
         return false;
       }
 /*---------------------------------------------------------------------------------------------------------------*/
     // Dealer spielt nachdem alle gespielt haben
-    Thread.sleep(1000);
-    System.out.println("Der Dealer hat "+dealerSecond+" Augen. \n");
+    System.out.println(printWait(1,7,dealerSecond,0));
 
     while(dealerSecond<17){
-      System.out.println("Der Dealer zieht eine Karte");
-      Thread.sleep(2000);
+      System.out.println(printWait(2,8,0,0));
       tmpDealer = cd.getCard();
       dealerSecond += tmpDealer;
       // Prüfung ob der Dealer mit einem Ass mehr als 21 Punkte hätte
       if(tmpDealer==11&&dealerSecond>21){
         dealerSecond -=10;
       }
-
-      System.out.println("Der Dealer hat eine "+tmpDealer+" erhalten. Der aktuelle Kartenwert beträgt "+dealerSecond+".\n");
+      System.out.println(printWait(1,1,tmpDealer,dealerSecond));
     }
 
     if(dealerSecond<22&&dealerSecond>player){
-      Thread.sleep(1000);
-      System.out.println("Der Dealer gewinnt mit "+dealerSecond+". Sie verlieren mit "+player+" Augen.\n\n");
+      System.out.println(printWait(2,6,dealerSecond, player));
       return false;
     }
     if (dealerSecond<22&&dealerSecond<player){
-      Thread.sleep(1000);
-      System.out.println("Der Dealer verliert mit "+dealerSecond+". Sie gewinnen mit "+player+" Augen.\n\n");
+      System.out.println(printWait(2,5,dealerSecond,player));
       return true;
     }
     if (dealerSecond<22&&dealerSecond==player){
-      Thread.sleep(1000);
-      System.out.println("Unentschieden. Beide haben "+dealerSecond+" Augen.\n\n");
+      System.out.println(printWait(1,12,dealerSecond,0));
       return true;
     }
     if(dealerSecond>21&&player<22){
-      Thread.sleep(1000);
-      System.out.println("Der Dealer verliert mit "+dealerSecond+". Sie gewinnen mit "+player+" Augen.\n\n");
+      System.out.println(printWait(2,5,dealerSecond,player));
       return true;
     }
     return true;
