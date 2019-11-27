5147c6ab46e1b9a54a9437bd5de783619de7ecbd
Stefan Ganze <stefan.ganze@googlemail.com>
2019-11-27 16:13:29 +0100

    Kartenbezeichnung  hinzugefügt

--- CardDeck.java
+++ CardDeck.java
@@ -2,23 +2,50 @@ import java.util.ArrayList;
 
 public class CardDeck {
 
+  int zufallsindex = 0;
+
   ArrayList<Integer> cards = new ArrayList<Integer> ();
+  ArrayList<String> cardsColors = new ArrayList<String>();
+
   int[] cardsNumbers = {2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,
         10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,
       11,11,11,11};
 
+  String[] color =
+  {
+  "Herz 2","Kreuz 2","Pik 2","Karo 2",
+  "Herz 3","Kreuz 3","Pik 3","Karo 3",
+  "Herz 4","Kreuz 4","Pik 4","Karo 4",
+  "Herz 5","Kreuz 5","Pik 5","Karo 5",
+  "Herz 6","Kreuz 6","Pik 6","Karo 6",
+  "Herz 7","Kreuz 7","Pik 7","Karo 7",
+  "Herz 8","Kreuz 8","Pik 8","Karo 8",
+  "Herz 9","Kreuz 9","Pik 9","Karo 9",
+  "Herz 10","Kreuz 10","Pik 10","Karo 10",
+  "Herz Bube","Kreuz Bube","Pik Bube","Karo Bube",
+  "Herz Dame","Kreuz Dame","Pik Dame","Karo Dame",
+  "Herz König","Kreuz König","Pik König","Karo König",
+  "Herz Ass","Kreuz Ass","Pik Ass","Karo Ass"
+  };
+
   public void createDeck (){
       for (int i=0; i<cardsNumbers.length; i++) {
       cards.add (cardsNumbers[i]);
+      cardsColors.add(color[i]);
     }
   }
 
   public int getCard () {
-    int zufallsindex = (int) (Math.random() * cards.size());
+    this.zufallsindex = (int) (Math.random() * cards.size());
     int newCard = cards.get(zufallsindex);
 
     cards.remove (zufallsindex);
-
     return newCard;
+  }
+
+  public String getCardColor(){
+    String tmpColor = cardsColors.get(zufallsindex);
+    cardsColors.remove(zufallsindex);
+    return tmpColor;
   }
 }
--- GameMaster.java
+++ GameMaster.java
@@ -14,11 +14,11 @@ public class GameMaster{
       return auswahl;
   }
 
-  public String printWait(int s, int str, int x, int y) throws Exception {
+  public String printWait(int s, int str, int x, int y, String color) throws Exception {
 
     String strDefault = "Fehler 404";
-    String str1 = "Der Dealer hat eine "+x+" erhalten. Der aktuelle Kartenwert des Dealers beträgt "+y+".";
-    String str2 = "Sie haben eine "+x+" erhalten. Ihr aktueller Kartenwert beträgt "+y+".\n";
+    String str1 = "Der Dealer hat eine "+x+" ("+color+") erhalten. Der aktuelle Kartenwert des Dealers beträgt "+y+".";
+    String str2 = "Sie haben eine "+x+" ("+color+") erhalten. Ihr aktueller Kartenwert beträgt "+y+".\n";
     String str3 = "Der Dealer hat eine verdeckte Karte erhalten. Der aktuelle Kartenwert des Dealers beträgt "+x+".";
     String str4 = "Herzlichen Glückwunsch - BlackJack!!!\n";
     String str5 = "Der Dealer verliert mit "+x+". Sie gewinnen mit "+y+" Augen.\n\n";
@@ -27,7 +27,7 @@ public class GameMaster{
     String str8 = "Der Dealer zieht eine Karte";
     String str9 = "Sie haben sich für eine "+x+" entschieden. Ihr neuer Kartenwert beträgt: "+y+"\n";
     String str10 = "Möchten sie weiter spielen? Ihr akuteller Kartenwert beträgt: "+x+ "\n\n1. Neue Karte! \n2. Keine Karte!\n";
-    String str11 = "Sie haben ein Ass erhalten. Soll es als 1 oder 11 gewertet werden?\n\n 1. Ass als 1 werten! \n2. Ass als 11 werten!\n";
+    String str11 = "Sie haben ein Ass ("+color+") erhalten. Soll es als 1 oder 11 gewertet werden?\n\n 1. Ass als 1 werten! \n2. Ass als 11 werten!\n";
     String str12 = "Unentschieden. Beide haben "+x+" Augen.\n\n";
 
     Thread.sleep(s*1000);
@@ -73,31 +73,31 @@ public class GameMaster{
 
     //Runde 1
     int tmpDealer = cd.getCard();
-    int tmpPlayer = cd.getCard();
-
     dealerFirst += tmpDealer;
     dealerSecond = dealerFirst;
-    player += tmpPlayer;
+    System.out.println(printWait(2,1,tmpDealer, dealerFirst,cd.getCardColor()));
 
-    System.out.println(printWait(2,1,tmpDealer, dealerFirst));
-    System.out.println(printWait(2,2, tmpPlayer, player));
+    int tmpPlayer = cd.getCard();
+    player += tmpPlayer;
+    System.out.println(printWait(2,2, tmpPlayer, player,cd.getCardColor()));
 
     //Runde 2
     tmpDealer = cd.getCard();
-    tmpPlayer = cd.getCard();
-
+    cd.getCardColor();
     dealerSecond += tmpDealer;
+    System.out.println(printWait(2,3,dealerFirst,0,""));
+
+    tmpPlayer = cd.getCard();
     player += tmpPlayer;
 
     if(player == 22){
       player=12;
     }
 
-    System.out.println(printWait(2,3,dealerFirst,0));
-    System.out.println(printWait(2,2,tmpPlayer, player));
+    System.out.println(printWait(2,2,tmpPlayer, player,cd.getCardColor()));
 
     if(player==21){
-      System.out.println(printWait(2,4,0,0));
+      System.out.println(printWait(2,4,0,0,""));
       return true;
     }
 
@@ -113,7 +113,7 @@ public class GameMaster{
 
     // Weiterspielen und neue Karte ziehen oder seinen Spielzug beenden
     while(auswahl!=2&&player<21){
-      System.out.println(printWait(1,10,player,0));
+      System.out.println(printWait(1,10,player,0,""));
       Scanner scanner = new Scanner(System.in);
       auswahl = scanner.nextInt();
       karte = cd.getCard();
@@ -121,63 +121,63 @@ public class GameMaster{
       //Der Spieler zieht eine neue Karte und es kein Ass
       if(auswahl == 1 && karte != 11){
         player += karte;
-        System.out.println(printWait(2,2,karte,player));
+        System.out.println(printWait(2,2,karte,player,cd.getCardColor()));
         }
 
       //Der Spieler zieht eine Karte und hat ein Ass gezogen
       if(auswahl == 1 && karte == 11){
-        System.out.println(printWait(1,11,0,0));
+        System.out.println(printWait(1,11,0,0,cd.getCardColor()));
         elevenOrOne  = scanner.nextInt();
 
         if(elevenOrOne == 1){
           player += 1;
-          System.out.println(printWait(1,9,1,player));
+          System.out.println(printWait(1,9,1,player,""));
         }
 
         if(elevenOrOne == 2){
           player += 11;
-          System.out.println(printWait(1,9,11,player));
+          System.out.println(printWait(1,9,11,player,""));
         }
       }
     }
 
       if(player == 21){
-        System.out.println(printWait(2,4,0,0));
+        System.out.println(printWait(2,4,0,0,""));
         return true;
       }
       if(player > 21){
-        System.out.println(printWait(2,6,dealerSecond, player));
+        System.out.println(printWait(2,6,dealerSecond, player,""));
         return false;
       }
 /*---------------------------------------------------------------------------------------------------------------*/
     // Dealer spielt nachdem alle gespielt haben
-    System.out.println(printWait(1,7,dealerSecond,0));
+    System.out.println(printWait(1,7,dealerSecond,0,""));
 
     while(dealerSecond<17){
-      System.out.println(printWait(2,8,0,0));
+      System.out.println(printWait(2,8,0,0,""));
       tmpDealer = cd.getCard();
       dealerSecond += tmpDealer;
       // Prüfung ob der Dealer mit einem Ass mehr als 21 Punkte hätte
       if(tmpDealer==11&&dealerSecond>21){
         dealerSecond -=10;
       }
-      System.out.println(printWait(1,1,tmpDealer,dealerSecond));
+      System.out.println(printWait(1,1,tmpDealer,dealerSecond,cd.getCardColor()));
     }
 
     if(dealerSecond<22&&dealerSecond>player){
-      System.out.println(printWait(2,6,dealerSecond, player));
+      System.out.println(printWait(2,6,dealerSecond, player,""));
       return false;
     }
     if (dealerSecond<22&&dealerSecond<player){
-      System.out.println(printWait(2,5,dealerSecond,player));
+      System.out.println(printWait(2,5,dealerSecond,player,""));
       return true;
     }
     if (dealerSecond<22&&dealerSecond==player){
-      System.out.println(printWait(1,12,dealerSecond,0));
+      System.out.println(printWait(1,12,dealerSecond,0,""));
       return true;
     }
     if(dealerSecond>21&&player<22){
-      System.out.println(printWait(2,5,dealerSecond,player));
+      System.out.println(printWait(2,5,dealerSecond,player,""));
       return true;
     }
     return true;
